92a4c9740d7452c5efee1a3bff744c4c4cad8af2
seriaati <seria.ati@gmail.com>
2023-09-07 15:02:00 +0800

    feat: reservation command

--- restaurant_bot/cogs/info.py
+++ restaurant_bot/cogs/info.py
@@ -13,14 +13,28 @@ class InfoCog(Cog):
         await ctx.reply_template(
             alt_text="店家資訊",
             template=ButtonsTemplate(
-                title="丹丹漢堡 (文慈店)",
-                text="地址: 813高雄市左營區文慈路192-1號\n營業時間: 7:00~21:30, 週二公休",
+                title="麥當勞-高雄博愛二餐廳",
+                text="地址: 813高雄市左營區博愛三路225號\n營業時間: 7:00~21:30, 週二公休",
                 actions=[
                     URIAction(
-                        label="Google 地圖", uri="https://goo.gl/maps/bLTS7sFZhe2TRr9D9"
+                        label="Google 地圖", uri="https://goo.gl/maps/YRpkfpQ1C1LEdLew8"
                     ),
-                    URIAction(label="致電", uri="tel:073469388"),
+                    URIAction(label="致電", uri="tel:073461756"),
                 ],
-                thumbnail_image_url="https://lh5.googleusercontent.com/p/AF1QipMQXLxVJkpAFG5A3T0oRSFOpsdOLdzdPdaAZ2_c=w408-h306-k-no",
+                thumbnail_image_url="https://lh5.googleusercontent.com/p/AF1QipOXKBPpqvEupr_MsCyFiBb6x2kTv6mIJAPu_d79=w408-h306-k-no",
+            ),
+        )
+
+    @command
+    async def reservation(self, ctx: Context) -> Any:
+        await ctx.reply_template(
+            alt_text="訂位",
+            template=ButtonsTemplate(
+                title="訂位",
+                text="目前採電話定位制",
+                actions=[
+                    URIAction(label="致電", uri="tel:073461756"),
+                ],
+                thumbnail_image_url="https://lh5.googleusercontent.com/p/AF1QipOXKBPpqvEupr_MsCyFiBb6x2kTv6mIJAPu_d79=w408-h306-k-no",
             ),
         )
