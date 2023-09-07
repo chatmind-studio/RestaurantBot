7cbea429bf210bc3acf7edb5501a77c252d6d3bf
seriaati <seria.ati@gmail.com>
2023-09-07 15:02:08 +0800

    upd: db models

--- restaurant_bot/models.py
+++ restaurant_bot/models.py
@@ -1,5 +1,5 @@
 from enum import StrEnum
-from typing import List
+from typing import List, Optional
 
 from tortoise import fields
 from tortoise.models import Model
@@ -8,21 +8,18 @@ from tortoise.models import Model
 class Coupon(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=20)
-    description = fields.CharField(max_length=100)
+    description = fields.CharField(max_length=50)
     expire_date = fields.DateField()
-    users: fields.ManyToManyRelation["User"]
 
 
 class User(Model):
     id = fields.CharField(pk=True, max_length=33)
     name = fields.CharField(max_length=255)
     points = fields.IntField(default=0, min_value=0)
-    coupons: fields.ManyToManyRelation[Coupon] = fields.ManyToManyField(
-        "models.Coupon", related_name="users", through="user_coupon"
-    )
     is_admin = fields.BooleanField(default=False)
     password = fields.CharField(max_length=255, null=True, default=None)
     cart: List[int] = fields.JSONField(default=[])  # type: ignore
+    coupon_ids: List[int] = fields.JSONField(default=[])  # type: ignore
 
 
 class ItemCategory(StrEnum):
@@ -38,4 +35,4 @@ class Item(Model):
     description = fields.CharField(max_length=50)
     category = fields.CharEnumField(ItemCategory, max_length=20)
     price = fields.IntField(min_value=0)
-    image_url = fields.CharField(max_length=255, null=True, default=None)
+    image_url: Optional[str] = fields.CharField(max_length=255, null=True, default=None)  # type: ignore
